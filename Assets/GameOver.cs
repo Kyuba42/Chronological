9aa95a165267809d37b6a9e94b3632332b8adedd
Corbin <mottramc@sky.com>
2020-02-01 23:09:14 +0000

    Added Restart Function to timer
    Should probs expand and also allow people to leave the level

---
+++ Assets/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startTime = 20f;
+    //if using timer
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        if(currentTime <= 0)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+    }
+}
