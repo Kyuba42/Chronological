78666856b06a41a7a621bd428c27240ca221b8e7
Seb <sebastianploch@protonmail.com>
2020-02-02 02:03:24 +0000

    Pickup highlights PickUp objects

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -7,36 +7,47 @@ public class PickUp : MonoBehaviour
     public Material outlineMaterialRef;
     private GameObject pickedUpObject;
     private Material[] objectMaterials;
+    private bool holdItem;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        holdItem = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check if LBM pressed
-        if (Input.GetMouseButtonDown(0))
+        RaycastHit rayHit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out rayHit, 10.0f))
         {
-            if (pickedUpObject == null)
-            {
-                RaycastHit rayHit;
-                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out rayHit, 6.0f))
-                {
-                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayHit.distance, Color.yellow);
-                    if (rayHit.collider.gameObject.tag == "PickUp")
-                    {
-                        pickedUpObject = rayHit.collider.gameObject;
-                        pickedUpObject.GetComponent<MeshRenderer>().material = outlineMaterialRef;
-                        rayHit.collider.gameObject.transform.parent = transform;
-                        pickedUpObject.GetComponent<Rigidbody>().useGravity = false;
-                        pickedUpObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
-                    }
-                }
-            }
-            else
+            //if (rayHit.collider.gameObject.tag == "PickUp")
+            //{
+            //    pickedUpObject = rayHit.collider.gameObject;
+            //    pickedUpObject.GetComponent<MeshRenderer>().material = outlineMaterialRef;
+
+            //    if (Input.GetMouseButtonDown(0))
+            //    {
+            //        if (!clicked)
+            //        {
+            //            rayHit.collider.gameObject.transform.parent = transform;
+            //            pickedUpObject.GetComponent<Rigidbody>().useGravity = false;
+            //            pickedUpObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+            //        }
+            //        else
+            //        {
+            //            pickedUpObject.transform.parent = null;
+            //            pickedUpObject.GetComponent<MeshRenderer>().material = pickedUpObject.GetComponent<MeshRenderer>().materials[1];
+            //            pickedUpObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            //            pickedUpObject.GetComponent<Rigidbody>().useGravity = true;
+            //            pickedUpObject = null;
+            //        }
+
+            //    }
+            //}
+            ///
+
+            if (holdItem == false && pickedUpObject != null)
             {
                 pickedUpObject.transform.parent = null;
                 pickedUpObject.GetComponent<MeshRenderer>().material = pickedUpObject.GetComponent<MeshRenderer>().materials[1];
@@ -44,7 +55,46 @@ public class PickUp : MonoBehaviour
                 pickedUpObject.GetComponent<Rigidbody>().useGravity = true;
                 pickedUpObject = null;
             }
+
+            // Object is a PickUp
+            if (rayHit.collider.gameObject.tag == "PickUp")
+            {
+                // Turn off outline if looking at another PickUp object
+                if (pickedUpObject != null && rayHit.collider.gameObject != pickedUpObject)
+                {
+                    pickedUpObject.GetComponent<MeshRenderer>().material = pickedUpObject.GetComponent<MeshRenderer>().materials[1];
+                }
+
+                // Grab Object that collided with the ray and apply outline
+                pickedUpObject = rayHit.collider.gameObject;
+                pickedUpObject.GetComponent<MeshRenderer>().material = outlineMaterialRef;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (!holdItem)
+                    {
+                        holdItem = true;
+
+                        pickedUpObject.GetComponent<Rigidbody>().useGravity = false;
+                        pickedUpObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+                        rayHit.collider.gameObject.transform.parent = transform;
+                    }
+                    else
+                    {
+                        holdItem = false;
+                    }
+                }
+            }
+            else
+            {
+                // Turn off outline if looking at other object than previous pickUp
+                if (pickedUpObject != null)
+                {
+                    pickedUpObject.GetComponent<MeshRenderer>().material = pickedUpObject.GetComponent<MeshRenderer>().materials[1];
+                }
+            }
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
         }
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
     }
 }
